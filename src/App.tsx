d7e04a459a138b94566c8fbdb9e1c3c4e8fb5639
Oskar <oskar0201@onet.pl>
2024-05-24 14:08:26 +0200

    updated project structure: main & app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import "./App.css"
+import {Converter} from "./components/Converter/Converter.tsx";
+import {RateTable} from "./components/RateTable/RateTable.tsx";
 
-import './App.css'
+const currencies = ['USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'PLN'];
+const currencyOptions = currencies.map(currency => ({ value: currency, label: currency }));
 
 export function App() {
 
-  return (
-    <>
-    </>
+    return (
+    <div className="app">
+        <header>
+            <h1>$ Currency Converter</h1>
+        </header>
+        <div className="flexContainer">
+            <Converter currencyOptions={currencyOptions}/>
+            <RateTable currencies={currencies} currencyOptions={currencyOptions}/>
+        </div>
+    </div>
   )
 }
